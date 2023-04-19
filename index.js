bb9ba9ec6c16f7b30164b928cdc7b0d395decbe7
nanda77 <nalandaneeraj.n7@gmail.com>
2023-04-19 22:57:35 +0530

    added dependency

--- index.js
+++ index.js
@@ -13,41 +13,68 @@ const verifiedUsers = [];
 // Configure the port that the server listens on
 const port = process.env.PORT || 3000;
 
+// Function to check if a user is verified
+function isVerified(user) {
+  return verifiedUsers.includes(user.id);
+}
+
+// Function to verify a user
+function verifyUser(user, chatId) {
+  bot.sendMessage(chatId, `Welcome to the group, ${user.first_name}! To verify that you are human, please solve this math problem: 5 + 3 = ?`)
+    .then(() => {
+      // Listen for the user's response
+      bot.on('message', (response) => {
+        const answer = parseInt(response.text);
+
+        // Check if the user's answer is correct
+        if (answer === 8) {
+          bot.sendMessage(chatId, 'Thank you, you are verified!');
+          verifiedUsers.push(user.id);
+          bot.restrictChatMember(chatId, user.id, {
+            can_send_messages: true,
+            can_send_media_messages: true,
+            can_send_polls: true,
+            can_send_other_messages: true,
+            can_add_web_page_previews: true,
+            can_change_info: true,
+            can_invite_users: true,
+            can_pin_messages: true
+          });
+        } else {
+          bot.sendMessage(chatId, 'Sorry, your answer is incorrect. Please try again.');
+        }
+
+        // Remove the listener to prevent multiple verifications
+        bot.removeListeners('message');
+      });
+    });
+}
+
 // Listen for new users joining the group
 bot.on('new_chat_members', (message) => {
   const user = message.new_chat_member;
   const chatId = message.chat.id;
 
   // Check if the user is already verified
-  if (verifiedUsers.includes(user.id)) {
+  if (isVerified(user)) {
     bot.sendMessage(chatId, `Welcome back, ${user.first_name}!`);
   } else {
-    // Send a welcome message and verification step to the new user
-    bot.sendMessage(chatId, `Welcome to the group, ${user.first_name}! To verify that you are human, please solve this math problem: 5 + 3 = ?`)
-      .then(() => {
-        // Listen for the user's response
-        bot.on('message', (response) => {
-          const answer = parseInt(response.text);
+    verifyUser(user, chatId);
+  }
+});
 
-          // Check if the user's answer is correct
-          if (answer === 8) {
-            bot.sendMessage(chatId, 'Thank you, you are verified!');
-            verifiedUsers.push(user.id);
-          } else {
-            bot.sendMessage(chatId, 'Sorry, your answer is incorrect. Please try again.');
-          }
+// Listen for messages sent in the group
+bot.on('message', (message) => {
+  const user = message.from;
+  const chatId = message.chat.id;
 
-          // Remove the listener to prevent multiple verifications
-          bot.removeListeners('message');
-
-          // Block the user from sending messages if they are not verified
-          if (!verifiedUsers.includes(user.id)) {
-            bot.restrictChatMember(chatId, user.id, {
-              can_send_messages: false
-            });
-          }
-        });
-      });
+  // Block the user from sending messages if they are not verified
+  if (!isVerified(user)) {
+    bot.deleteMessage(chatId, message.message_id);
+    bot.restrictChatMember(chatId, user.id, {
+      can_send_messages: false
+    });
+    verifyUser(user, chatId);
   }
 });
 
