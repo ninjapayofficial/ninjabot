ea534908d9fb1387c2ebbb8a65efb4eb029b74b2
nanda77 <nalandaneeraj.n7@gmail.com>
2023-04-20 15:30:59 +0530

    added captcha

--- index.js
+++ index.js
@@ -22,26 +22,29 @@ const app = express();
 
 const TOKEN = process.env.TELEGRAM_TOKEN || '6248027615:AAFGqxA_SPOTcyKv18SMQ9e_ERYfEHe4SQs';
 
-const { GroupCaptcha } = require('telegram-captcha');
+// Use dynamic import for 'telegram-captcha'
+(async () => {
+  const { GroupCaptcha } = await import('telegram-captcha');
 
-const captcha = new GroupCaptcha(TOKEN, { polling: true }, {
-  size: 6,
-  language: 'de',
-  time_for_enter: 3,
-});
+  const captcha = new GroupCaptcha(TOKEN, { polling: true }, {
+    size: 6,
+    language: 'de',
+    time_for_enter: 3,
+  });
 
-captcha.bot.on('new_chat_members', (msg) => captcha.generateCaptcha(msg));
-captcha.bot.on('callback_query', (query) => captcha.clickKeyboard(query));
+  captcha.bot.on('new_chat_members', (msg) => captcha.generateCaptcha(msg));
+  captcha.bot.on('callback_query', (query) => captcha.clickKeyboard(query));
 
-// Sample endpoint
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+  // Sample endpoint
+  app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
 
-// Configure the port
-const port = process.env.PORT || 3000;
+  // Configure the port
+  const port = process.env.PORT || 3000;
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})();
