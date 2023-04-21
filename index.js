90ec793baccf8abe38021ac62668a2d7e5fe58a2
nanda77 <nalandaneeraj.n7@gmail.com>
2023-04-21 17:37:54 +0530

    added lightning

--- index.js
+++ index.js
@@ -1,51 +1,61 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+const axios = require('axios');
+const QRCode = require('qrcode');
+const webhook = require('./webhook');
+const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.TELEGRAM_TOKEN || '6248027615:AAFGqxA_SPOTcyKv18SMQ9e_ERYfEHe4SQs';
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-app.use(morgan('combined'));
+app.use(express.json());
+app.use(webhook.router);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+bot.on('new_chat_members', async (msg) => {
+  try {
+    await bot.restrictChatMember(msg.chat.id, msg.new_chat_member.id, {
+      can_send_messages: false,
+      can_send_media_messages: false,
+      can_send_polls: false,
+      can_send_other_messages: false,
+      can_add_web_page_previews: false,
+      can_change_info: false,
+      can_invite_users: false,
+      can_pin_messages: false,
+    });
+
+    const invoiceResponse = await axios.post(
+      'https://legend.lnbits.com/api/v1/payments',
+      {
+        out: false,
+        amount: 100,
+        memo: 'Telegram group access',
+        webhook: 'https://your-webhook-url.com/webhook',
+      },
+      {
+        headers: {
+          'X-Api-Key': 'c6bda6e5c9374c21a5cdee58572f08e1',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    const qrCode = await QRCode.toDataURL(invoiceResponse.data.payment_request);
+
+    webhook.addPendingPayment(invoiceResponse.data.payment_hash, msg.chat.id, msg.new_chat_member.id);
+
+    await bot.sendPhoto(msg.chat.id, qrCode, {
+      caption: 'Please pay the 100 SAT invoice to get access to the chat',
+    });
+  } catch (error) {
+    console.error('Error handling new chat member:', error);
+  }
 });
 
-(async () => {
-  const { GroupCaptcha } = await import('telegram-captcha');
+// Configure the port
+const port = process.env.PORT || 3000;
 
-  const captcha = new GroupCaptcha(TOKEN, { polling: true }, {
-    size: 6,
-    language: 'en',
-    time_for_enter: 3,
-  });
-
-  captcha.bot.on('new_chat_members', async (msg) => {
-    try {
-      await captcha.generateCaptcha(msg);
-    } catch (error) {
-      console.error('Error generating captcha:', error);
-    }
-  });
-
-  captcha.bot.on('callback_query', async (query) => {
-    try {
-      await captcha.clickKeyboard(query);
-    } catch (error) {
-      console.error('Error handling callback query:', error);
-    }
-  });
-
-  // Sample endpoint
-  app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-
-  // Configure the port
-  const port = process.env.PORT || 3000;
-
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-})();
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ webhook.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_TOKEN';
+const bot = new TelegramBot(TOKEN);
+
+const pendingPayments = new Map();
+
+router.post('/webhook', async (req, res) => {
+  try {
+    const paymentHash = req.body.payment_hash;
+
+    if (!paymentHash) {
+      res.status(400).send('Missing payment hash');
+      return;
+    }
+
+    const paymentInfo = pendingPayments.get(paymentHash);
+
+    if (!paymentInfo) {
+      res.status(404).send('Payment not found');
+      return;
+    }
+
+    const { chatId, userId } = paymentInfo;
+    await bot.restrictChatMember(chatId, userId, {
+      can_send_messages: true,
+      can_send_media_messages: true,
+      can_send_polls: true,
+      can_send_other_messages: true,
+      can_add_web_page_previews: true,
+      can_change_info: false,
+      can_invite_users: false,
+      can_pin_messages: false,
+    });
+
+    pendingPayments.delete(paymentHash);
+    res.status(200).send('Payment processed successfully');
+  } catch (error) {
+    console.error('Error processing payment webhook:', error);
+    res.status(500).send('Error processing payment webhook');
+  }
+});
+
+function addPendingPayment(paymentHash, chatId, userId) {
+  pendingPayments.set(paymentHash, { chatId, userId });
+}
+
+module.exports = {
+  router,
+  addPendingPayment,
+};
