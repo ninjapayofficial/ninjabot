374e42c58b178881cb6983975a068d9bfddefa45
nanda77 <nalandaneeraj.n7@gmail.com>
2023-04-21 18:28:08 +0530

    added lightning

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const QRCode = require('qrcode');
-const webhook = require('./webhook');
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = process.env.TELEGRAM_TOKEN || '6248027615:AAFGqxA_SPOTcyKv18SMQ9e_ERYfEHe4SQs';
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 app.use(express.json());
-app.use(webhook.router);
+
+const payments = {};
 
 bot.on('new_chat_members', async (msg) => {
   try {
@@ -30,7 +30,6 @@ bot.on('new_chat_members', async (msg) => {
         out: false,
         amount: 100,
         memo: 'Telegram group access',
-        webhook: 'https://evening-escarpment-94174.herokuapp.com/webhook',
       },
       {
         headers: {
@@ -41,10 +40,50 @@ bot.on('new_chat_members', async (msg) => {
     );
 
     const paymentRequest = invoiceResponse.data.payment_request;
+    const paymentHash = invoiceResponse.data.payment_hash;
 
-    webhook.addPendingPayment(invoiceResponse.data.payment_hash, msg.chat.id, msg.new_chat_member.id);
+    payments[paymentHash] = {
+      chatId: msg.chat.id,
+      memberId: msg.new_chat_member.id,
+      paymentRequest: paymentRequest,
+      paid: false,
+    };
 
     await bot.sendMessage(msg.chat.id, `Please pay the 100 SAT invoice to get access to the chat: \n${paymentRequest}`);
+
+    // Start checking payment status periodically
+    setInterval(async () => {
+      try {
+        const paymentStatusResponse = await axios.get(`https://legend.lnbits.com/api/v1/payments/${paymentHash}`, {
+          headers: {
+            'X-Api-Key': 'c6bda6e5c9374c21a5cdee58572f08e1',
+            'Content-type': 'application/json'
+          }
+        });
+
+        const paid = paymentStatusResponse.data.paid;
+        const payment = payments[paymentHash];
+
+        if (paid && !payment.paid) {
+          // Grant chat access
+          await bot.restrictChatMember(payment.chatId, payment.memberId, {
+            can_send_messages: true,
+            can_send_media_messages: true,
+            can_send_polls: true,
+            can_send_other_messages: true,
+            can_add_web_page_previews: true,
+            can_change_info: true,
+            can_invite_users: true,
+            can_pin_messages: true,
+          });
+
+          await bot.sendMessage(payment.chatId, `Payment received. Welcome to the group!`);
+          payment.paid = true;
+        }
+      } catch (error) {
+        console.error(`Error checking payment status for ${paymentHash}:`, error);
+      }
+    }, 10000); // Check every 10 seconds
   } catch (error) {
     console.error('Error handling new chat member:', error);
   }
---
+++ indexlnwithqr.js
@@ -0,0 +1,61 @@
+// const express = require('express');
+// const app = express();
+// const axios = require('axios');
+// const QRCode = require('qrcode');
+// const webhook = require('./webhook');
+// const TelegramBot = require('node-telegram-bot-api');
+
+// const TOKEN = process.env.TELEGRAM_TOKEN || '6248027615:AAFGqxA_SPOTcyKv18SMQ9e_ERYfEHe4SQs';
+// const bot = new TelegramBot(TOKEN, { polling: true });
+
+// app.use(express.json());
+// app.use(webhook.router);
+
+// bot.on('new_chat_members', async (msg) => {
+//   try {
+//     await bot.restrictChatMember(msg.chat.id, msg.new_chat_member.id, {
+//       can_send_messages: false,
+//       can_send_media_messages: false,
+//       can_send_polls: false,
+//       can_send_other_messages: false,
+//       can_add_web_page_previews: false,
+//       can_change_info: false,
+//       can_invite_users: false,
+//       can_pin_messages: false,
+//     });
+
+//     const invoiceResponse = await axios.post(
+//       'https://legend.lnbits.com/api/v1/payments',
+//       {
+//         out: false,
+//         amount: 100,
+//         memo: 'Telegram group access',
+//         webhook: 'https://evening-escarpment-94174.herokuapp.com/webhook',
+//       },
+//       {
+//         headers: {
+//           'X-Api-Key': 'c6bda6e5c9374c21a5cdee58572f08e1',
+//           'Content-Type': 'application/json',
+//         },
+//       }
+//     );
+
+//     const qrCode = await QRCode.toDataURL(invoiceResponse.data.payment_request);
+
+//     webhook.addPendingPayment(invoiceResponse.data.payment_hash, msg.chat.id, msg.new_chat_member.id);
+
+//     await bot.sendPhoto(msg.chat.id, qrCode, {
+//       caption: 'Please pay the 100 SAT invoice to get access to the chat',
+//     });
+//   } catch (error) {
+//     console.error('Error handling new chat member:', error);
+//   }
+// });
+
+// // Configure the port
+// const port = process.env.PORT || 3000;
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
