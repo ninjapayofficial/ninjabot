ec7ac3c47b530265910ac8ab88b2569a72128436
nanda77 <nalandaneeraj.n7@gmail.com>
2023-04-22 11:15:59 +0530

    added qr and delete message after payment

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 app.use(express.json());
 
 const payments = {};
-let sentHelloMessage;
-let sentInvoiceMessage;
 
 bot.on('new_chat_members', async (msg) => {
   try {
@@ -126,6 +124,8 @@ bot.on('new_chat_members', async (msg) => {
 // Configure the port
 bot.on('callback_query', async (query) => {
   let paymentHash;
+  let sentHelloMessage;
+  let sentInvoiceMessage;
   const { message, data } = query;
   const chatId = message.chat.id;
   try {
